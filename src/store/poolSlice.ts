6a5edaa04c1717fb57fbfc93d1283092ecc56105
Ryan Yang <ryanyang2001@gmail.com>
2023-10-28 19:50:16 -0500

    fix: update oracle price fetching for new oracle interface

--- src/store/poolSlice.ts
+++ src/store/poolSlice.ts
@@ -1,8 +1,15 @@
-import * as Pool from '@blend-capital/blend-sdk/pool';
-import { Address, Server, xdr } from 'soroban-client';
+import {
+  PoolConfig,
+  PoolUserEmissionData,
+  PoolUserEmissions,
+  Reserve,
+  UserPositions,
+} from '@blend-capital/blend-sdk';
+import { Address, Server, nativeToScVal, scValToBigInt, scValToNative, xdr } from 'soroban-client';
 import { StateCreator } from 'zustand';
 import { getTokenBalance } from '../external/token';
 import { DataStore, useStore } from './store';
+
 export type ReserveBalance = {
   asset: bigint;
   collateral: bigint;
@@ -10,14 +17,14 @@ export type ReserveBalance = {
 };
 
 export type PoolData = {
-  reserves: Pool.Reserve[];
+  reserves: Reserve[];
   poolPrices: Map<string, number>;
   lastUpdated: number;
 };
 
 export type PoolUserData = {
   reserveBalances: Map<string, ReserveBalance>;
-  emissionsData: Map<number, Pool.UserEmissionData>;
+  emissionsData: Map<number, PoolUserEmissionData>;
   totalEmissions: bigint;
   lastUpdated: number;
 };
@@ -38,7 +45,7 @@ export type UserReserveEmission = {
  * Ledger state for a set of pools
  */
 export interface PoolSlice {
-  pools: Map<string, Pool.PoolConfig>;
+  pools: Map<string, PoolConfig>;
   poolData: Map<string, PoolData>;
   poolUserData: Map<string, PoolUserData>;
 
@@ -47,7 +54,7 @@ export interface PoolSlice {
 }
 
 export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set, get) => ({
-  pools: new Map<string, Pool.PoolConfig>(),
+  pools: new Map<string, PoolConfig>(),
   poolData: new Map<string, PoolData>(),
   poolUserData: new Map<string, PoolUserData>(),
 
@@ -60,17 +67,16 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
       let pool = get().pools.get(pool_id);
       let set_pool = false;
       if (pool == undefined) {
-        pool = await Pool.PoolConfig.load({ rpc, passphrase, opts: { allowHttp: true } }, pool_id);
+        pool = await PoolConfig.load({ rpc, passphrase, opts: { allowHttp: true } }, pool_id);
         set_pool = true;
       }
       const prices = await loadOraclePrices(stellar, pool_id, pool);
-      const pool_reserves: Pool.Reserve[] = [];
+      const pool_reserves: Reserve[] = [];
       for (const assetId of pool.reserveList) {
         pool_reserves.push(
-          await Pool.Reserve.load({ rpc, passphrase, opts: { allowHttp: true } }, pool_id, assetId)
+          await Reserve.load({ rpc, passphrase, opts: { allowHttp: true } }, pool_id, assetId)
         );
       }
-      console.log(pool_reserves, pool);
       if (set_pool) {
         useStore.setState((prev) => ({
           poolData: new Map(prev.poolData).set(pool_id, {
@@ -78,7 +84,7 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
             reserves: pool_reserves,
             lastUpdated: latest_ledger_close,
           }),
-          pools: new Map(prev.pools).set(pool_id, pool as Pool.PoolConfig),
+          pools: new Map(prev.pools).set(pool_id, pool as PoolConfig),
         }));
       } else {
         useStore.setState((prev) => ({
@@ -90,7 +96,7 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
         }));
       }
     } catch (e) {
-      console.error(`unable to refresh data for pool ${pool_id}`);
+      console.error(`unable to refresh data for pool ${pool_id}, ${e}`);
     }
   },
 
@@ -105,7 +111,7 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
         throw Error('unknown pool');
       }
 
-      const user_reserve_positions = await Pool.UserPositions.load(
+      const user_reserve_positions = await UserPositions.load(
         { rpc, passphrase, opts: { allowHttp: true } },
         pool_id,
         user
@@ -126,7 +132,7 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
       }
 
       let total_user_emissions = BigInt(0);
-      let userEmissions = await Pool.UserEmissions.load(
+      let userEmissions = await PoolUserEmissions.load(
         { rpc, passphrase, opts: { allowHttp: true } },
         pool_id,
         user,
@@ -137,7 +143,6 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
       for (let entry of Array.from(userEmissions.emissions.entries())) {
         total_user_emissions += entry[1].accrued;
       }
-      console.log(userReserveBalances, userEmissions, total_user_emissions);
       useStore.setState((prev) => ({
         poolUserData: new Map(prev.poolUserData).set(pool_id, {
           reserveBalances: userReserveBalances,
@@ -155,29 +160,102 @@ export const createPoolSlice: StateCreator<DataStore, [], [], PoolSlice> = (set,
 async function loadOraclePrices(
   stellar: Server,
   poolId: string,
-  pool: Pool.PoolConfig
+  pool: PoolConfig
 ): Promise<Map<string, number>> {
   let price_map = new Map<string, number>();
-  let decimals = 7;
-  let priceLedgerKeys = pool.reserveList.map((reserve) => {
-    let price_datakey = xdr.ScVal.scvVec([
-      xdr.ScVal.scvSymbol('Prices'),
-      Address.fromString(reserve).toScVal(),
-    ]);
+  let decimals: number;
+  const oracleInstanceDataKey = xdr.LedgerKey.contractData(
+    new xdr.LedgerKeyContractData({
+      contract: Address.fromString(pool.oracle).toScAddress(),
+      key: xdr.ScVal.scvLedgerKeyContractInstance(),
+      durability: xdr.ContractDataDurability.persistent(),
+    })
+  );
+  let oracleInstanceEntries = (await stellar.getLedgerEntries(oracleInstanceDataKey)).entries ?? [];
 
-    return xdr.LedgerKey.contractData(
-      new xdr.LedgerKeyContractData({
-        contract: Address.fromString(poolId).toScAddress(),
-        key: price_datakey,
-        durability: xdr.ContractDataDurability.persistent(),
-      })
-    );
-  });
+  let priceLedgerKeys: xdr.LedgerKey[] = [];
+  let indexToAssetIdMapping: Map<number, string> = new Map();
+  for (const entry of oracleInstanceEntries) {
+    const ledgerData = xdr.LedgerEntryData.fromXDR(entry.xdr, 'base64').contractData();
+    let key: string;
+    switch (ledgerData.key().switch()) {
+      // Key is a ScVec[ScvSym, ScVal]
+      case xdr.ScValType.scvVec():
+        key = ledgerData.key().vec()?.at(0)?.sym().toString() ?? 'Void';
+        break;
+      case xdr.ScValType.scvSymbol():
+        key = ledgerData.key().sym().toString();
+        break;
+      case xdr.ScValType.scvLedgerKeyContractInstance():
+        key = 'ContractInstance';
+        break;
+      case xdr.ScValType.scvAddress():
+        key = Address.fromScVal(ledgerData.key()).toString();
+        break;
+      default:
+        key = 'Void';
+    }
+    switch (key) {
+      case 'ContractInstance':
+        ledgerData
+          .val()
+          .instance()
+          .storage()
+          ?.map((entry) => {
+            let instanceKey: string;
+            switch (ledgerData.key().switch()) {
+              // Key is a ScVec[ScvSym, ScVal]
+              case xdr.ScValType.scvVec():
+                instanceKey = entry.val().vec()?.at(0)?.sym().toString() ?? 'Void';
+                break;
+              case xdr.ScValType.scvSymbol():
+                instanceKey = entry.val().sym().toString();
+                break;
+              case xdr.ScValType.scvAddress():
+                instanceKey = Address.fromScVal(ledgerData.key()).toString();
+                break;
+              default:
+                instanceKey = 'Void';
+            }
+            switch (instanceKey) {
+              case 'assets':
+                entry
+                  .val()
+                  .vec()
+                  ?.forEach((vec, index) => {
+                    let address = scValToNative(vec)[1];
+                    if (address != undefined && pool.reserveList.includes(address)) {
+                      indexToAssetIdMapping.set(index, address);
+                      priceLedgerKeys.push(
+                        xdr.LedgerKey.contractData(
+                          new xdr.LedgerKeyContractData({
+                            contract: Address.fromString(pool.oracle).toScAddress(),
+                            key: nativeToScVal(index, { type: 'u128' }),
+                            durability: xdr.ContractDataDurability.temporary(),
+                          })
+                        )
+                      );
+                    }
+                  });
+                break;
+              case 'decimals':
+                decimals = entry.val().u32();
+            }
+          });
+        break;
+    }
+  }
 
   let priceLedgerEntries = (await stellar.getLedgerEntries(...priceLedgerKeys)).entries ?? [];
   for (const entry of priceLedgerEntries) {
     const ledgerData = xdr.LedgerEntryData.fromXDR(entry.xdr, 'base64').contractData();
-    console.log(ledgerData.key());
+    let index = scValToBigInt(ledgerData.key());
+    let price = scValToBigInt(ledgerData.val());
+    let assetId = indexToAssetIdMapping.get(Number(index)) ?? undefined;
+    if (assetId != undefined) {
+      price_map.set(assetId, Number(price) / 10 ** 7);
+    }
   }
+  console.log(price_map);
   return price_map;
 }
