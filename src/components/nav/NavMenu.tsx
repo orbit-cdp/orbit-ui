532c39d460770037eee820111c5f655ad8ca20db
Ryan Yang <ryanyang2001@gmail.com>
2023-11-08 12:15:53 -0600

    feat: add faucet in nav menu

--- src/components/nav/NavMenu.tsx
+++ src/components/nav/NavMenu.tsx
@@ -1,17 +1,25 @@
 import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 import { IconButton, Menu, MenuItem, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { Asset, Transaction, xdr } from 'soroban-client';
 import { ViewType, useSettings } from '../../contexts';
+import { useWallet } from '../../contexts/wallet';
 import { useStore } from '../../store/store';
 import { NavItem } from './NavItem';
 
 export const NavMenu = () => {
   const theme = useTheme();
+  const network = useStore((state) => state.network);
+  let account = useStore((state) => state.account);
+  const hasTrustline = useStore((state) => state.hasTrustline);
+  const loadAccount = useStore((state) => state.loadAccount);
+  const loadPoolData = useStore((state) => state.loadPoolData);
   const { viewType, lastPool } = useSettings();
   const rewardZone = useStore((state) => state.backstopConfig.rewardZone);
   const [poolId, setPoolId] = useState<string>(lastPool ?? '');
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+  const { connected, faucet, walletAddress } = useWallet();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -28,6 +36,39 @@ export const NavMenu = () => {
       setPoolId(rewardZone.length != 0 ? rewardZone[0] : '');
     }
   }, [rewardZone, lastPool]);
+
+  const handleFaucet = async () => {
+    if (connected) {
+      const url = `https://ewqw4hx7oa.execute-api.us-east-1.amazonaws.com/getAssets?userId=${walletAddress}`;
+      if (account == undefined) {
+        account = await loadAccount(walletAddress);
+      }
+      try {
+        if (
+          !hasTrustline(
+            new Asset('USDC', 'GCDUQQ2LP2M32Q563YOJOG36KXO5T635FKSWG4IQWYFE2FQHMMQKYK3S'),
+            account
+          ) ||
+          !hasTrustline(
+            new Asset('BLND', 'GCDUQQ2LP2M32Q563YOJOG36KXO5T635FKSWG4IQWYFE2FQHMMQKYK3S'),
+            account
+          )
+        ) {
+          const resp = await fetch(url, { method: 'GET' });
+          const txEnvelopeXDR = (await resp.json()) as { type: string; data: number[] };
+          await faucet(
+            new Transaction(
+              xdr.TransactionEnvelope.fromXDR(Buffer.from(txEnvelopeXDR.data)),
+              network.passphrase
+            )
+          );
+          await loadPoolData(poolId, walletAddress, true);
+        }
+      } catch (e) {
+        console.error('Faucet Failed', e);
+      }
+    }
+  };
 
   return (
     <>
@@ -55,6 +96,7 @@ export const NavMenu = () => {
           <MenuItem onClick={handleClose}>Docs</MenuItem>
           <MenuItem onClick={handleClose}>User agreement</MenuItem>
           <MenuItem onClick={handleClose}>Privacy policy</MenuItem>
+          <MenuItem onClick={handleFaucet}>Faucet</MenuItem>
         </Menu>
       )}
       {viewType !== ViewType.REGULAR && (
