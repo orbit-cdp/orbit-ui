35998b257382dfbcdb9094cd4e32810418f0bc46
Alex Mootz <alexmootz@hotmail.com>
2023-11-15 16:42:04 -0500

    chore: remove faucet from navbar

--- src/components/nav/NavMenu.tsx
+++ src/components/nav/NavMenu.tsx
@@ -1,22 +1,15 @@
 import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 import { Alert, IconButton, Menu, MenuItem, Snackbar, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { Asset, Transaction, xdr } from 'soroban-client';
-import { ViewType, useSettings } from '../../contexts';
-import { useWallet } from '../../contexts/wallet';
+import { useSettings, ViewType } from '../../contexts';
 import { useStore } from '../../store/store';
-import { requiresTrustline } from '../../utils/horizon';
 import { NavItem } from './NavItem';
 
 export const NavMenu = () => {
   const theme = useTheme();
   const { viewType, lastPool } = useSettings();
-  const { connected, faucet, walletAddress } = useWallet();
 
-  const network = useStore((state) => state.network);
-  const account = useStore((state) => state.account);
   const rewardZone = useStore((state) => state.backstopConfig.rewardZone);
-  const loadPoolData = useStore((state) => state.loadPoolData);
 
   const [poolId, setPoolId] = useState<string>(lastPool ?? '');
   const [openCon, setOpenCon] = React.useState(false);
@@ -44,38 +37,6 @@ export const NavMenu = () => {
     }
   }, [rewardZone, lastPool]);
 
-  const handleFaucet = async () => {
-    if (connected) {
-      const url = `https://ewqw4hx7oa.execute-api.us-east-1.amazonaws.com/getAssets?userId=${walletAddress}`;
-      try {
-        if (
-          requiresTrustline(
-            account,
-            new Asset('USDC', 'GCDUQQ2LP2M32Q563YOJOG36KXO5T635FKSWG4IQWYFE2FQHMMQKYK3S')
-          ) ||
-          requiresTrustline(
-            account,
-            new Asset('BLND', 'GCDUQQ2LP2M32Q563YOJOG36KXO5T635FKSWG4IQWYFE2FQHMMQKYK3S')
-          )
-        ) {
-          const resp = await fetch(url, { method: 'GET' });
-          const txEnvelopeXDR = (await resp.json()) as { type: string; data: number[] };
-          await faucet(
-            new Transaction(
-              xdr.TransactionEnvelope.fromXDR(Buffer.from(txEnvelopeXDR.data)),
-              network.passphrase
-            )
-          );
-          loadPoolData(poolId, walletAddress, true);
-        } else {
-          setOpenCon(true);
-        }
-      } catch (e) {
-        console.error('Faucet Failed', e);
-      }
-    }
-  };
-
   return (
     <>
       <IconButton
@@ -102,7 +63,6 @@ export const NavMenu = () => {
           <MenuItem onClick={handleClose}>Docs</MenuItem>
           <MenuItem onClick={handleClose}>User agreement</MenuItem>
           <MenuItem onClick={handleClose}>Privacy policy</MenuItem>
-          <MenuItem onClick={handleFaucet}>Faucet</MenuItem>
         </Menu>
       )}
       {viewType !== ViewType.REGULAR && (
@@ -140,7 +100,6 @@ export const NavMenu = () => {
           <MenuItem onClick={handleClose}>Docs</MenuItem>
           <MenuItem onClick={handleClose}>User agreement</MenuItem>
           <MenuItem onClick={handleClose}>Privacy policy</MenuItem>
-          <MenuItem onClick={handleFaucet}>Faucet</MenuItem>
         </Menu>
       )}
 
