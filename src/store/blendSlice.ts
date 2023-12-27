428f346a8baa55b09ab28cd4bc035e9e23a98f81
mootz12 <mootz12@protonmail.com>
2023-12-27 15:41:27 -0500

    chore: update store to use new sdk logic

---
+++ src/store/blendSlice.ts
@@ -0,0 +1,83 @@
+import { Backstop, Pool } from '@blend-capital/blend-sdk';
+import { StateCreator } from 'zustand';
+import { DataStore } from './store';
+
+const BACKSTOP_ID = 'CCK5UFOXYMP4YP2YCMTT3EWW262Z4BUQASWHA6J7ZGVEARRFTH4EQXJ5';
+
+/**
+ * Ledger state for the Blend protocol
+ */
+export interface BlendSlice {
+  backstop: Backstop | undefined;
+  pools: Map<string, Pool>;
+  latestLedger: number;
+  latestLedgerTimestamp: number;
+
+  loadBlendData: (force_update: boolean, pool_id?: string, user_id?: string) => Promise<void>;
+}
+
+export const createBlendSlice: StateCreator<DataStore, [], [], BlendSlice> = (set, get) => ({
+  backstop: undefined,
+  pools: new Map<string, Pool>(),
+  latestLedger: 0,
+  latestLedgerTimestamp: 0,
+
+  loadBlendData: async (force_update: boolean, pool_id?: string, user_id?: string) => {
+    try {
+      const network = get().network;
+
+      // get latest ledger close time (TODO: File issue to include close time on getLatestLedger)
+      const rpc = get().rpcServer();
+      let tx_response = await rpc.getTransaction(
+        '0000000000000000000000000000000000000000000000000000000000000000'
+      );
+      let latest_ledger_close = tx_response.latestLedgerCloseTime;
+
+      if (
+        !force_update &&
+        get().backstop !== undefined &&
+        latest_ledger_close < get().latestLedgerTimestamp + 55
+      ) {
+        return;
+      }
+
+      console.time('loadAllData');
+      console.time('loadBlendData');
+
+      let backstop = await Backstop.load(
+        network,
+        BACKSTOP_ID,
+        pool_id ? [pool_id] : [],
+        true,
+        latest_ledger_close
+      );
+
+      // all pools in the reward zone + the request pools are loaded on the backstop
+      let pools = new Map<string, Pool>();
+      for (let pool of Array.from(backstop.pools.keys())) {
+        let pool_data = await Pool.load(network, pool, latest_ledger_close);
+        pools.set(pool, pool_data);
+      }
+
+      console.log(Array.from(pools.entries()));
+
+      set({
+        backstop,
+        pools,
+        latestLedger: latest_ledger_close,
+        latestLedgerTimestamp: Date.now(),
+      });
+
+      // load data into user slice after an updated, if a user is specified
+      if (user_id) {
+        console.time('loadUserData');
+        await get().loadUserData(user_id);
+        console.timeEnd('loadUserData');
+      }
+      console.timeEnd('loadAllData');
+    } catch (e) {
+      console.error('Unable to load Blend data');
+      console.error(e);
+    }
+  },
+});
---
+++ src/store/userSlice.ts
@@ -0,0 +1,104 @@
+import { BackstopUser, PoolUser } from '@blend-capital/blend-sdk';
+import { Address, Horizon } from 'stellar-sdk';
+import { StateCreator } from 'zustand';
+import { getTokenBalance } from '../external/token';
+import { DataStore } from './store';
+
+/**
+ * Ledger state for the Blend protocol
+ */
+export interface UserSlice {
+  account: Horizon.AccountResponse | undefined;
+  balances: Map<string, bigint>;
+  backstopUserData: BackstopUser | undefined;
+  userPoolData: Map<string, PoolUser>;
+
+  loadUserData: (id: string) => Promise<void>;
+  clearUserData: () => void;
+}
+
+export const createUserSlice: StateCreator<DataStore, [], [], UserSlice> = (set, get) => ({
+  account: undefined,
+  balances: new Map<string, bigint>(),
+  backstopUserData: undefined,
+  userPoolData: new Map<string, PoolUser>(),
+
+  loadUserData: async (id: string) => {
+    try {
+      const network = get().network;
+      const rpc = get().rpcServer();
+
+      const backstop = get().backstop;
+      const pools = get().pools;
+
+      if (backstop == undefined || pools.size == 0) {
+        throw new Error('Backstop or pool data not loaded');
+      }
+
+      // load horizon account
+      let horizon = get().horizonServer();
+      let account = await horizon.loadAccount(id);
+
+      // load user data for backstop
+      let backstop_user = await backstop.loadUser(network, id);
+
+      // load pool data for user for each tracked pool
+      // load token balances for each unique reserve or fetch from the account response
+      let user_pool_data = new Map<string, PoolUser>();
+      let user_balances = new Map<string, bigint>();
+      for (let [pool, pool_data] of Array.from(pools.entries())) {
+        let pool_user = await pool_data.loadUser(network, id);
+        user_pool_data.set(pool, pool_user);
+
+        for (let reserve of Array.from(pool_data.reserves.values())) {
+          if (user_balances.has(reserve.assetId)) {
+            // duplicate reserve from another pool, skip
+            continue;
+          }
+
+          if (reserve.tokenMetadata.asset != undefined) {
+            // stellar asset, fetch balance from account response
+            let balance_line = account.balances.find((balance) => {
+              if (balance.asset_type == 'native') {
+                // @ts-ignore
+                return reserve.tokenMetadata.asset.isNative();
+              }
+              return (
+                // @ts-ignore
+                balance.asset_code === asset.getCode() && balance.asset_issuer === asset.getIssuer()
+              );
+            });
+            let balance_string = balance_line ? balance_line.balance.replace('.', '') : '0';
+            user_balances.set(reserve.assetId, BigInt(balance_string));
+          } else {
+            let balance = await getTokenBalance(
+              rpc,
+              network.passphrase,
+              reserve.assetId,
+              new Address(id)
+            );
+            user_balances.set(reserve.assetId, balance);
+          }
+        }
+      }
+
+      set({
+        account,
+        balances: user_balances,
+        backstopUserData: backstop_user,
+        userPoolData: user_pool_data,
+      });
+    } catch (e) {
+      console.error('Unable to load user data');
+      console.error(e);
+    }
+  },
+  clearUserData: () => {
+    set({
+      account: undefined,
+      balances: new Map<string, bigint>(),
+      backstopUserData: undefined,
+      userPoolData: new Map<string, PoolUser>(),
+    });
+  },
+});
