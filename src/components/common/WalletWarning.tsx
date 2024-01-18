b7ec3b831f4dd31614fdc807aa72e974623e5023
mootz12 <mootz12@protonmail.com>
2024-01-18 13:20:01 -0500

    fix: add banner if wallet account has not been funded

--- src/components/common/WalletWarning.tsx
+++ src/components/common/WalletWarning.tsx
@@ -1,23 +1,60 @@
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { Alert, Box, Snackbar, Typography, useTheme } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useWallet } from '../../contexts/wallet';
+import { useStore } from '../../store/store';
+import { toCompactAddress } from '../../utils/formatter';
 import { OpaqueButton } from './OpaqueButton';
+import { Row } from './Row';
 
 export const WalletWarning = () => {
   const theme = useTheme();
-  const { connect, connected } = useWallet();
+  const { connect, connected, walletAddress } = useWallet();
+  const isFunded = useStore((state) => state.isFunded);
+  const loadUserData = useStore((state) => state.loadUserData);
+
   const [openCon, setOpenCon] = React.useState(false);
 
   const handleSnackClose = () => {
     setOpenCon(false);
   };
 
+  useEffect(() => {
+    if (connected && !isFunded) {
+      loadUserData(walletAddress);
+      const refreshInterval = setInterval(async () => {
+        await loadUserData(walletAddress);
+      }, 3 * 1000);
+      return () => clearInterval(refreshInterval);
+    }
+  }, [loadUserData, connected, isFunded, walletAddress]);
+
   return (
     <>
       {connected ? (
-        <></>
+        !isFunded ? (
+          <Row
+            sx={{
+              background: theme.palette.warning.opaque,
+              color: theme.palette.warning.main,
+              display: 'flex',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              margin: '6px',
+              padding: '12px',
+              paddingRight: '20px',
+            }}
+          >
+            <InfoOutlinedIcon sx={{ marginRight: '6px' }} />
+            <Typography variant="body2">
+              The wallet address {toCompactAddress(walletAddress)} does not exist on the network.
+              Please fund your account!
+            </Typography>
+          </Row>
+        ) : (
+          <></>
+        )
       ) : (
         <OpaqueButton
           onClick={() => {
