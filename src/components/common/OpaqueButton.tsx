34efde86006a16d227176464c210d290865c3e19
Carlos Di Matteo <carlosdima97@gmail.com>
2024-01-22 10:05:10 -0500

    fix next Link ref error

--- src/components/common/OpaqueButton.tsx
+++ src/components/common/OpaqueButton.tsx
@@ -2,11 +2,19 @@ import { Button, ButtonProps, PaletteColor } from '@mui/material';
 
 export interface OpaqueButtonProps extends ButtonProps {
   palette: PaletteColor;
+  passedRef: any;
 }
 
-export const OpaqueButton: React.FC<OpaqueButtonProps> = ({ children, palette, sx, ...props }) => {
+export const OpaqueButton: React.FC<OpaqueButtonProps> = ({
+  children,
+  palette,
+  sx,
+  passedRef,
+  ...props
+}) => {
   return (
     <Button
+      ref={passedRef}
       variant="contained"
       sx={{
         background: palette.opaque,
