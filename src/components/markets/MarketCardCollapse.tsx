07ac452527db3500f0dbbc501fcbce955960a810
mootz12 <mootz12@protonmail.com>
2024-05-22 09:25:28 -0400

    chore: add better pool details and link backstop token

--- src/components/markets/MarketCardCollapse.tsx
+++ src/components/markets/MarketCardCollapse.tsx
@@ -40,12 +40,7 @@ export const MarketCardCollapse: React.FC<MarketCardCollapseProps> = ({
       }}
       {...props}
     >
-      <Row
-        sx={{
-          flex: 'flex',
-          flexDirection: viewType === ViewType.REGULAR ? 'row' : 'column',
-        }}
-      >
+      <Row>
         <OpaqueButton
           palette={theme.palette.accent}
           sx={{
@@ -77,6 +72,13 @@ export const MarketCardCollapse: React.FC<MarketCardCollapseProps> = ({
             </Box>
           </Box>
         </OpaqueButton>
+      </Row>
+      <Row
+        sx={{
+          flex: 'flex',
+          flexDirection: viewType === ViewType.REGULAR ? 'row' : 'column',
+        }}
+      >
         <OpaqueButton
           palette={theme.palette.accent}
           sx={{
@@ -101,6 +103,36 @@ export const MarketCardCollapse: React.FC<MarketCardCollapseProps> = ({
           <Box sx={{ padding: '6px', display: 'flex', flexDirection: 'row', height: '30px' }}>
             <Box sx={{ paddingRight: '12px', lineHeight: '100%' }}>{`Oracle ${toCompactAddress(
               poolData.config.oracle
+            )}`}</Box>
+            <Box>
+              <OpenInNewIcon fontSize="inherit" />
+            </Box>
+          </Box>
+        </OpaqueButton>
+        <OpaqueButton
+          palette={theme.palette.accent}
+          sx={{
+            width: SectionSize.FULL,
+            margin: '6px',
+            padding: '6px',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            color: theme.palette.text.secondary,
+            cursor: 'default',
+          }}
+          onClick={() =>
+            window.open(
+              `${process.env.NEXT_PUBLIC_STELLAR_EXPERT_URL}/contract/${poolData.config.oracle}`,
+              '_blank'
+            )
+          }
+        >
+          <Box sx={{ margin: '6px', height: '30px' }}>
+            <Icon src={'/icons/blend_logo.svg'} alt="oracle-icon" isCircle={false} />
+          </Box>
+          <Box sx={{ padding: '6px', display: 'flex', flexDirection: 'row', height: '30px' }}>
+            <Box sx={{ paddingRight: '12px', lineHeight: '100%' }}>{`Admin ${toCompactAddress(
+              poolData.config.admin
             )}`}</Box>
             <Box>
               <OpenInNewIcon fontSize="inherit" />
@@ -161,13 +193,18 @@ export const MarketCardCollapse: React.FC<MarketCardCollapseProps> = ({
               </Row>
               <Row>
                 <StackedTextBox
-                  name="Backstop APY"
+                  name="APY"
                   text={toPercentage(estBackstopApy)}
                   sx={{ width: '50%' }}
                 />
                 <StackedTextBox
                   name="Q4W"
                   text={toPercentage(backstopPoolData.estimates.q4wPercentage)}
+                  sx={{ width: '50%', color: theme.palette.backstop.main }}
+                />
+                <StackedTextBox
+                  name="Take Rate"
+                  text={toPercentage(poolData.config.backstopRate / 1e7)}
                   sx={{ width: '50%', color: theme.palette.backstop.main }}
                 />
               </Row>
